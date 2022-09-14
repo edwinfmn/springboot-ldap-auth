d8b53a227941858a4d155eb86ba98c52ac847dba
Edwin Martinez <edwin.martinez@fisagrp.com>
2022-09-14 18:53:45 -0500

    Demo

--- complete/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
+++ complete/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.userDnPatterns("uid={0},ou=people")
 				.groupSearchBase("ou=groups")
 				.contextSource()
-					.url("ldap://localhost:8389/dc=springframework,dc=org")
+					.url("ldap://localhost:8390/dc=springframework,dc=org")
 					.and()
 				.passwordCompare()
 					.passwordEncoder(new BCryptPasswordEncoder())
